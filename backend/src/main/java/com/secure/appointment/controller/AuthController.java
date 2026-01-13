a17560c0a924ef266dbe0cb504e658807310a182
Sahil <sahildeshmukh.3008@gmail.com>
2026-01-13 16:36:19 +0530

    Files Updates

--- backend/src/main/java/com/secure/appointment/controller/AuthController.java
+++ backend/src/main/java/com/secure/appointment/controller/AuthController.java
@@ -10,9 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
-@RestController
-@RequestMapping("/api/auth")
+//@CrossOrigin(origins = "*", maxAge = 3600)
+//@RestController
+//@RequestMapping("/api/auth")
 /**
  * AuthController: The Entry Gate for Authentication.
  * 
